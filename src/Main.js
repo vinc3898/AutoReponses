c24bbaeef0b235b4de0c4e4539ebb608451d5794
vinc3898 <vintan.1013@gmail.com>
2022-05-22 20:59:24 +0800

    Enhanced user interface

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 const form = FormApp.getActiveForm();
 
 function onOpen(){
-  
   FormApp.getUi().createAddonMenu().addItem("Open", "showSidebar").addToUi();
 }
 
@@ -36,4 +35,31 @@ function result(){
     }
   });
   return data;
+}
+
+function openUrl( url ){
+  var html = HtmlService.createHtmlOutput('<html><script>'
+  +'window.close = function(){window.setTimeout(function(){google.script.host.close()},9)};'
+  +'var a = document.createElement("a"); a.href="'+url+'"; a.target="_blank";'
+  +'if(document.createEvent){'
+  +'  var event=document.createEvent("MouseEvents");'
+  +'  if(navigator.userAgent.toLowerCase().indexOf("firefox")>-1){window.document.body.append(a)}'                          
+  +'  event.initEvent("click",true,true); a.dispatchEvent(event);'
+  +'}else{ a.click() }'
+  +'close();'
+  +'</script>'
+  // Offer URL as clickable link in case above code fails.
+  +'<body style="word-break:break-word;font-family:sans-serif;">Failed to open automatically. <a href="'+url+'" target="_blank" onclick="window.close()">Click here to proceed</a>.</body>'
+  +'<script>google.script.host.setHeight(40);google.script.host.setWidth(410)</script>'
+  +'</html>')
+  .setWidth( 90 ).setHeight( 1 );
+  FormApp.getUi().showModalDialog( html, "Opening ..." );
+}
+
+function open(){
+  for(let i = 0;i<5;i++){
+    Logger.log(i);
+      openUrl("https://docs.google.com/forms/u/4/d/e/1FAIpQLSfT2SVnCbT233B6M71dvaDErvxiUSC3njSAjA99Ka-QVdA9dg/formResponse?usp=pp_url&entry.1193869602=%E9%80%89%E9%A1%B9+1&entry.1592361129=%E9%80%89%E9%A1%B9+1&submit=Submit")
+      Utilities.sleep(5000);
+  }
 }
